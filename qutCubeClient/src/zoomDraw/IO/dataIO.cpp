b6c9e232fce7e0e5b55e822201934433438ffb07
zach lieberman <zach@eyebeam.org>
2014-12-04 22:15:47 +1000

    fix for win

--- qutCubeClient/src/zoomDraw/IO/dataIO.cpp
+++ qutCubeClient/src/zoomDraw/IO/dataIO.cpp
@@ -45,29 +45,42 @@ void dataIO::loadData(string fileName){
     
     
     ofApp * app = (ofApp * ) ofGetAppPtr();
-
     
     
-//    NSString * path = [NSString stringWithUTF8String: (ofxiOSGetDocumentsDirectory() + fileName + "/" ).c_str() ];
-//    path = [path stringByAppendingPathComponent:@"lineSettings.plist"];
-//    NSArray * settings = [[NSArray alloc] initWithContentsOfFile:path];
-//    NSLog(@"array: %@", settings);
+    
+    ifstream myfile;
+    myfile.open ( ofToDataPath("example.txt").c_str());
+    string lineStr;
+    
+    //    NSString * path = [NSString stringWithUTF8String: (ofxiOSGetDocumentsDirectory() + fileName + "/" ).c_str() ];
+    //    path = [path stringByAppendingPathComponent:@"lineSettings.plist"];
+    //    NSArray * settings = [[NSArray alloc] initWithContentsOfFile:path];
+    //    NSLog(@"array: %@", settings);
     
     //string filePathName = ofxiOSGetDocumentsDirectory() + fileName;
-    string filePathName = fileName;
+    //string filePathName = fileName;
     
-    ofBuffer buffer = ofBufferFromFile(filePathName);
-    char * buff  = buffer.getBinaryBuffer();            // get the raw data;
+    //ofBuffer buffer = ofBufferFromFile(filePathName);
+    //char * buff  = buffer.getBinaryBuffer();            // get the raw data;
+    getline(myfile,lineStr);
+    app->scale = ofToFloat(lineStr); //*((float *)(buff));                     //TODO: nice to have this animate a lil, not a lot...
+    //buff += sizeof(float);
+    //myfile << app->scale << endl;
     
-    app->scale =*((float *)(buff));                     //TODO: nice to have this animate a lil, not a lot...
-    buff += sizeof(float);
     
     
+    //
+    getline(myfile,lineStr);
     //app->cam.setZoom(*((float *)(buff)));
-    buff += sizeof(float);
+    //myfile << (float)*((float *)(buff)) << endl;
+    //buff += sizeof(float);
     
-    int nLines = *((int *)(buff));
-    buff += sizeof(int);
+    
+    getline(myfile,lineStr);
+    int nLines = ofToInt(lineStr); //*((int *)(buff));
+    //myfile << (int)*((int *)(buff)) << endl;
+    //buff += sizeof(int);
+    
     
     app->currentLines.clear();
     
@@ -75,10 +88,16 @@ void dataIO::loadData(string fileName){
         
         linesWithScale line;
         
-        float scale =  *((float *)(buff));
-        buff += sizeof(float);
-        int nPoints =  *((int *)(buff));
-        buff += sizeof(int);
+        
+        getline(myfile,lineStr);
+        float scale =  ofToFloat(lineStr); //*((float *)(buff));
+        //buff += sizeof(float);
+        //myfile << scale << endl;
+        
+        getline(myfile,lineStr);
+        int nPoints =  ofToInt(lineStr); //*((int *)(buff));
+        //buff += sizeof(int);
+        //myfile << nPoints << endl;
         
         line.scale = scale;
         
@@ -86,20 +105,37 @@ void dataIO::loadData(string fileName){
             
             drawnPoint DP;
             
-            float x =  *((float *)(buff));
-            buff += sizeof(float);
-            float y =  *((float *)(buff));
-            buff += sizeof(float);
             
-            float nx =  *((float *)(buff));
-            buff += sizeof(float);
-            float ny =  *((float *)(buff));
-            buff += sizeof(float);
             
-            float w =  *((float *)(buff));
-            buff += sizeof(float);
-            int imp =  *((int *)(buff));
-            buff += sizeof(int);
+            getline(myfile,lineStr);
+            float x = ofToFloat(lineStr); // *((float *)(buff));
+            //buff += sizeof(float);
+            
+            getline(myfile,lineStr);
+            float y =  ofToFloat(lineStr); //*((float *)(buff));
+            //buff += sizeof(float);
+            
+            getline(myfile,lineStr);
+            float nx = ofToFloat(lineStr); // *((float *)(buff));
+            //buff += sizeof(float);
+            getline(myfile,lineStr);
+            float ny = ofToFloat(lineStr); // *((float *)(buff));
+            //buff += sizeof(float);
+            
+            getline(myfile,lineStr);
+            float w =  ofToFloat(lineStr); //*((float *)(buff));
+            //buff += sizeof(float);
+            getline(myfile,lineStr);
+            int imp = ofToFloat(lineStr); // *((int *)(buff));
+            //buff += sizeof(int);
+            
+            //            myfile << x << endl;
+            //            myfile << y << endl;
+            //            myfile << nx << endl;
+            //            myfile << ny << endl;
+            //            myfile << w << endl;
+            //            myfile << imp << endl;
+            
             
             DP.pos.set(x,y);
             DP.normal.set(nx, ny);
@@ -115,11 +151,16 @@ void dataIO::loadData(string fileName){
         line.computeTexCoords();                    // <----------------- should this be loaded not computed?
         app->currentLines.push_back(line);
         
+        
     }
+    
+    myfile.close();
+    
     
     
     
 }
+
 
 
 
